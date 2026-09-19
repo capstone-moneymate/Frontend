1e1c34fa764ad0865c34092c7ea60366331ca873
hyunmin1756 <wowangela1756@gmail.com>
2026-09-19 17:55:58 +0900

    refactor: default export를 named export로 통일 (#3)

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router'
-import HomePage from '@/pages/HomePage'
+import { HomePage } from '@/pages/HomePage'
 
 export const router = createBrowserRouter([
   {
